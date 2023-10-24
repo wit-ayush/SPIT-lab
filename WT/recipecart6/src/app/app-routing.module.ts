04955cc6d224c3d864bf90c409a717ff3e67ccff
Ayush Tripathi <ayush.tripathi.8757@gmail.com>
2023-10-24 10:37:10 +0530

    WT-lab-6-top-tabs-added

--- WT/recipecart6/src/app/app-routing.module.ts
+++ WT/recipecart6/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeComponent } from './recipe/recipe.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'recipe', component: RecipeComponent },
+  // { path: 'cart', component: CartComponent },
+  { path: '', redirectTo: '/recipe', pathMatch: 'full' }, // Default route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WT/recipecart6/src/app/app.module.ts
+++ WT/recipecart6/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';  
+import { MatTabsModule } from '@angular/material/tabs';
+import { RecipeComponent } from './recipe/recipe.component';
+import { CartComponent } from './cart/cart.component';  
+      
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,  
+    MatTabsModule, 
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
