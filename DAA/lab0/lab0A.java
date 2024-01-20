570c8d6eb4b46cd1d5ff33ce0bd67672c68f7955
Ayush Tripathi <ayush.tripathi.8757@gmail.com>
2024-01-21 02:29:11 +0530

    DAA-lab0A-generalized

--- DAA/lab0/lab0A.java
+++ DAA/lab0/lab0A.java
@@ -2,47 +2,50 @@ import java.util.*;
 
 public class lab0A {
     public static void main(String args[]) {
-      int[][] matrix1 = new int[3][3];
-      int[][] matrix2 = new int[3][3];
-      int[][] ansMatrix = new int[3][3];
+      int n = 2;
+      int m = 3;
+      int p = 5;
+      
+      int[][] matrix1 = new int[n][m];
+      int[][] matrix2 = new int[m][p];
+      int[][] ansMatrix = new int[n][p];
       int r = 60;
-      int n = 3;
       int x = 0;
-      for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++)
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++)
           matrix1[i][j] = r+(r+1)*(x++);
       }
-      for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++)
+      for(int i = 0; i < m; i++){
+        for(int j = 0; j < p; j++)
           matrix2[i][j] = r+(r+1)*(x++);
       }
 
       System.out.println("Matrix 1");
-      for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
           System.out.print(matrix1[i][j] + "  ");
         }
         System.out.println(" ");
       }
       System.out.println("Matrix 2");
-      for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
+      for(int i = 0; i < m; i++){
+        for(int j = 0; j < p; j++){
           System.out.print(matrix2[i][j] + "  ");
         }
         System.out.println(" ");
       }
 
-      for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-          for(int k = 0; k < 3; k++){
-            // System.out.println("k: " + k + " j: " + j);
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < p; j++){
+          for(int k = 0; k < m; k++){
             ansMatrix[i][j] += matrix1[i][k]*matrix2[k][j];
           }
         }
       }
 
-      for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
+      System.out.println("Answer Matrix");
+      for(int i = 0; i < n; i++){
+        for(int j = 0; j < p; j++){
           System.out.print(ansMatrix[i][j] + "  ");
         }
         System.out.println(" ");
